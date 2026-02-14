eb5376f79efa7879da9e3ecbeed349e0d69043b8
muminul2027 <muminul2027developer@proton.me>
2026-02-14 22:25:31 +0600

    Add files via upload

---
+++ src/package/__init__.py
@@ -0,0 +1,13 @@
+
+"""
+
+#### Part of: Data_Structure_Algorithm_05
+
+#### Handle: _MUMINUL__ISLAM___
+
+"""
+
+#Actual Code Started Here
+#After New Python Update, This file is optional
+
+#Status : Complete
